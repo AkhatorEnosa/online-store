9c06ca2ca41ad6d7a261a7c714f66fafb9f2e763
AkhatorEnosa <aosakhogba@gmail.com>
2022-09-14 17:06:05 +0100

    initial commit for Hot deals component

--- src/components/Hot.jsx
+++ src/components/Hot.jsx
@@ -3,16 +3,19 @@ import Shoe  from '../assets/shoe.png';
 
 const Hot = () => {
   return (
-    <div className='w-full h-screen grid grid-cols-2'>
-      <div className='bg-red-900 mr-2'>
-        <div className=''>
-          <img src={Shoe} alt="shoe" />
-          <h1>Hot Sale</h1>
-          <h2>50% off</h2>
-          <p><span className='line-through'>₦30,000</span> ₦15,0000</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum placeat numquam ipsam dolor ut, voluptate voluptas voluptatem deserunt eligendi itaque omnis optio fuga provident dignissimos debitis tempore rem tenetur repudiandae!</p>
-          <button className='px-3 py-2 bg-[#c67019] w-44'>Buy Now</button>
-        </div>
+    <div className='w-full h-screen flex flex-col'>
+        <div className='z-10 w-full h-full p-5'>
+          <h1 className='bg-[#ff1c1c] text-white rounded-br-lg shadow-md w-fit px-2 py-2 pr-20 tracking-widest uppercase'>Hot Sale</h1>
+          <div className="flex">
+            <p className='my-5 uppercase text-[250px] font-black flex items-center'>50<span className='text-[50px] font-light text-[#ff1c1c]'>% </span></p>
+            <img src={Shoe} alt="shoe" className='-scale-x-100 ml-28 w-[600px] z-10'/>
+          </div>
+
+          <div className='mt-16'>
+          <p className='text-2xl z-20 font-light'><span className='line-through text-gray-500 opacity-75'>₦30,000</span> ₦15,0000</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum placeat numquam ipsam dolor ut, voluptate voluptas voluptatem deserunt eligendi itaque omnis optio fuga provident dignissimos debitis tempore rem tenetur repudiandae!</p>
+            <button className='px-8 py-2 rounded-md hover:bg-[#ff1c1c] bg-[#ff9123] text-white my-4 duration-300'>Buy Now</button>
+          </div>
       </div>
 
       <div className='grid grid-cols-2'>

25651a3d66aa775e5728dc7cf75f48ffa05c3716
AkhatorEnosa <aosakhogba@gmail.com>
2022-10-14 17:10:26 +0100

    bug fixes

--- src/components/Sections.jsx
+++ src/components/Sections.jsx
@@ -5,11 +5,11 @@ import {AiOutlineStar} from 'react-icons/ai';
 
 const Sections = () => {
   return (
-    <div className='grid md:grid-cols-3 grid-cols-2 md:p-6 p-2 pb-3'>
+    <div className='grid md:grid-cols-2 grid-cols-2 md:p-6 p-8 pb-3'>
       <div className="flex flex-col md:m-6 -mb-2 md:p-4 p-2 -pb-5 w-full md:border-l-2 border-yellow-200/20">
         <h1 className='uppercase border-b-2 border-[#fed243] w-fit pr-5 mb-10 '>Hot Trend</h1>
 
-        <div className="flex md:flex-row flex-col mb-12 md:items-start align-middle justify-center md:justify-start items-center text-center">
+        <div className="flex mb-12 items-start justify-start text-center">
             <img src={Bag} alt="item" className='md:w-24 w-32 md:h-24'/>
             <div className='flex flex-col justify-between md:m-0 md:ml-10 mt-5 md:self-stretch'>
               <span className='text-sm'>School bag</span>
@@ -24,8 +24,7 @@ const Sections = () => {
             </div>
         </div>
 
-
-        <div className="flex md:flex-row flex-col mb-12 md:items-start align-middle justify-center md:justify-start  items-center text-center">
+        <div className="flex mb-12 items-start justify-start text-center">
             <img src={Bag} alt="item" className='md:w-24 w-32 md:h-24'/>
             <div className='flex flex-col justify-between md:m-0 md:ml-10 mt-5 md:self-stretch'>
               <span className='text-sm'>School bag</span>
@@ -40,8 +39,7 @@ const Sections = () => {
             </div>
         </div>
 
-
-        <div className="flex md:flex-row flex-col mb-12 md:items-start align-middle justify-center md:justify-start  items-center text-center">
+        <div className="flex mb-12 items-start justify-start text-center">
             <img src={Bag} alt="item" className='md:w-24 w-32 md:h-24'/>
             <div className='flex flex-col justify-between md:m-0 md:ml-10 mt-5 md:self-stretch'>
               <span className='text-sm'>School bag</span>
@@ -61,7 +59,7 @@ const Sections = () => {
       <div className="flex flex-col md:m-6 -mb-2 md:p-4 p-2 -pb-5 w-full md:border-l-2 border-yellow-200/20">
         <h1 className='uppercase border-b-2 border-[#fed243] w-fit pr-5 mb-10 '>Best Sellers</h1>
 
-        <div className="flex md:flex-row flex-col mb-12 md:items-start align-middle justify-center md:justify-start items-center text-center">
+        <div className="flex mb-12 items-start justify-start text-center">
             <img src={Bag} alt="item" className='md:w-24 w-32 md:h-24'/>
             <div className='flex flex-col justify-between md:m-0 md:ml-10 mt-5 md:self-stretch'>
               <span className='text-sm'>School bag</span>
@@ -76,8 +74,7 @@ const Sections = () => {
             </div>
         </div>
 
-
-        <div className="flex md:flex-row flex-col mb-12 md:items-start align-middle justify-center md:justify-start  items-center text-center">
+        <div className="flex mb-12 items-start justify-start text-center">
             <img src={Bag} alt="item" className='md:w-24 w-32 md:h-24'/>
             <div className='flex flex-col justify-between md:m-0 md:ml-10 mt-5 md:self-stretch'>
               <span className='text-sm'>School bag</span>
@@ -92,60 +89,7 @@ const Sections = () => {
             </div>
         </div>
 
-
-        <div className="flex md:flex-row flex-col mb-12 md:items-start align-middle justify-center md:justify-start  items-center text-center">
-            <img src={Bag} alt="item" className='md:w-24 w-32 md:h-24'/>
-            <div className='flex flex-col justify-between md:m-0 md:ml-10 mt-5 md:self-stretch'>
-              <span className='text-sm'>School bag</span>
-              <div className='text-small flex'>
-                <AiFillStar className='text-[#fed243]'/>
-                <AiFillStar className='text-[#fed243]'/>
-                <AiFillStar className='text-[#fed243]'/>
-                <AiFillStar className='text-[#fed243]'/>
-                <AiOutlineStar/>
-                </div>
-              <p className='font-bold text-md'>₦10,500</p>
-            </div>
-        </div>
-      </div>
-      
-
-      <div className="flex flex-col md:m-6 -mb-2 md:p-4 p-2 -pb-5 w-full md:border-l-2 border-yellow-200/20">
-        <h1 className='uppercase border-b-2 border-[#fed243] w-fit pr-5 mb-10 '>Featured</h1>
-
-        <div className="flex md:flex-row flex-col mb-12 md:items-start align-middle justify-center md:justify-start items-center text-center">
-            <img src={Bag} alt="item" className='md:w-24 w-32 md:h-24'/>
-            <div className='flex flex-col justify-between md:m-0 md:ml-10 mt-5 md:self-stretch'>
-              <span className='text-sm'>School bag</span>
-              <div className='text-small flex'>
-                <AiFillStar className='text-[#fed243]'/>
-                <AiFillStar className='text-[#fed243]'/>
-                <AiFillStar className='text-[#fed243]'/>
-                <AiFillStar className='text-[#fed243]'/>
-                <AiOutlineStar/>
-                </div>
-              <p className='font-bold text-md'>₦10,500</p>
-            </div>
-        </div>
-
-
-        <div className="flex md:flex-row flex-col mb-12 md:items-start align-middle justify-center md:justify-start  items-center text-center">
-            <img src={Bag} alt="item" className='md:w-24 w-32 md:h-24'/>
-            <div className='flex flex-col justify-between md:m-0 md:ml-10 mt-5 md:self-stretch'>
-              <span className='text-sm'>School bag</span>
-              <div className='text-small flex'>
-                <AiFillStar className='text-[#fed243]'/>
-                <AiFillStar className='text-[#fed243]'/>
-                <AiFillStar className='text-[#fed243]'/>
-                <AiFillStar className='text-[#fed243]'/>
-                <AiOutlineStar/>
-                </div>
-              <p className='font-bold text-md'>₦10,500</p>
-            </div>
-        </div>
-
-
-        <div className="flex md:flex-row flex-col mb-12 md:items-start align-middle justify-center md:justify-start  items-center text-center">
+        <div className="flex mb-12 items-start justify-start text-center">
             <img src={Bag} alt="item" className='md:w-24 w-32 md:h-24'/>
             <div className='flex flex-col justify-between md:m-0 md:ml-10 mt-5 md:self-stretch'>
               <span className='text-sm'>School bag</span>

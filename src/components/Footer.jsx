f6a85628e337e906e6977716c1232af4622f0e0d
AkhatorEnosa <aosakhogba@gmail.com>
2022-10-14 16:33:14 +0100

    footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,29 +1,31 @@
 import React from 'react';
-import {AiFillCar} from 'react-icons/ai';
-import {FaMoneyBill} from 'react-icons/fa';
-import {BsHeadphones} from 'react-icons/bs';
-import {FaUserShield} from 'react-icons/fa';
+import {GrFacebookOption} from 'react-icons/gr';
+import {GrTwitter} from 'react-icons/gr';
+import {GrInstagram} from 'react-icons/gr';
+import {GrPinterest} from 'react-icons/gr';
+import {RiVisaLine} from 'react-icons/ri';
+import {FaCcMastercard} from 'react-icons/fa';
+import {FaCcPaypal} from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <div className='w-full flex justify-around p-10 pt-0'>
-      <div className='flex flex-col bg-slate-500 max-w-[450px] px-5 py-2'>
+    <div className='w-full flex md:flex-row flex-col justify-around p-10'>
+      <div className='flex flex-col max-w-[450px] md:px-5 py-2 mb-5'>
         <div className="logo text-black text-5xl font-extrabold items-center mb-6">
           <p>Shaup.com</p>
         </div>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt sequi nostrum at explicabo. Ullam tempora quisquam vel non!</p>
-          <div className="flex">
-            <AiFillCar className='text-[#3b3521]' size={60}/>
-            <AiFillCar className='text-[#3b3521]' size={60}/>
-            <AiFillCar className='text-[#3b3521]' size={60}/>
-            <AiFillCar className='text-[#3b3521]' size={60}/>
+          <p className='font-extralight text-sm'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt sequi nostrum at explicabo. Ullam tempora quisquam vel non!</p>
+          <div className="flex justify-between w-48 mt-5">
+            <RiVisaLine className='text-[#3b3521]' size={60}/>
+            <FaCcMastercard className='text-[#3b3521]' size={60}/>
+            <FaCcPaypal className='text-[#3b3521]' size={60}/>
           </div>
       </div>
 
-      <div className="flex flex-col bg-red-500 px-5 py-2">
-        <h1 className='text-1xl font-bold uppercase mb-4'>Quick Links</h1>
+      <div className="flex flex-col md:px-5 py-2">
+        <h1 className='text-1xl font-bold uppercase mb-2'>Quick Links</h1>
         <div className="flex flex-col">
-          <ul className='font-extralight'>
+          <ul className='font-extralight text-sm'>
             <li className='my-2'>About</li>
             <li className='my-2'>Blogs</li>
             <li className='my-2'>Contact</li>
@@ -32,10 +34,10 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className="flex flex-col bg-red-500 px-5 py-2">
-        <h1 className='text-1xl font-bold uppercase mb-4'>Account</h1>
+      <div className="flex flex-col md:px-5 py-2">
+        <h1 className='text-1xl font-bold uppercase mb-2'>Account</h1>
         <div className="flex flex-col">
-          <ul className='font-extralight'>
+          <ul className='font-extralight text-sm'>
             <li className='my-2'>About</li>
             <li className='my-2'>Blogs</li>
             <li className='my-2'>Contact</li>
@@ -44,26 +46,26 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className="flex flex-col bg-red-500 py-2 pl-2 pr-10">
-        <h1 className='text-1xl font-bold uppercase mb-4'>Newsletter</h1>
-        <div className="flex border-[1px] border-[#ccc] rounded-full py-1 px-2">
-          <input type="text" placeholder='Email' className='mr-6'/>
+      <div className="flex flex-col py-2 md:pl-2 pr-10">
+        <h1 className='text-1xl font-bold uppercase mb-2'>Newsletter</h1>
+        <div className="flex border-[1px] border-[#ccc] rounded-full py-1 px-2 w-fit">
+          <input type="text" placeholder='Email' className='mr-6 w-80 md:w-auto'/>
           <button className="bg-black text-white font-sm py-2 px-5 rounded-full">
             Subscribe
           </button>
           </div>
           <div className="flex mt-7">
-            <span className='bg-[#fed243] rounded-full p-2 mx-2'>
-              <AiFillCar className='text-[#3b3521]' size={20}/>
+            <span className='bg-[#fed243] rounded-full p-2 mx-2 cursor-pointer'>
+              <GrFacebookOption className='text-[#3b3521]' size={20}/>
             </span>
             <span className='bg-[#fed243] rounded-full p-2 mx-2'>
-              <AiFillCar className='text-[#3b3521]' size={20}/>
+              <GrTwitter className='text-[#3b3521]' size={20}/>
             </span>
             <span className='bg-[#fed243] rounded-full p-2 mx-2'>
-              <AiFillCar className='text-[#3b3521]' size={20}/>
+              <GrInstagram className='text-[#3b3521]' size={20}/>
             </span>
             <span className='bg-[#fed243] rounded-full p-2 mx-2'>
-              <AiFillCar className='text-[#3b3521]' size={20}/>
+              <GrPinterest className='text-[#3b3521]' size={20}/>
             </span>
         </div>
       </div>

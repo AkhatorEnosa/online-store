48f2d4f97b81a5782d13d381167d3e3e00e04c07
AkhatorEnosa <aosakhogba@gmail.com>
2022-10-26 14:40:09 +0100

    reconstruction of Hero section to fit the rest of the website nicely

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import Shoe from '../assets/shoe.png';
 const Hero = () => {
   return (
     <div className='w-full h-auto md:h-screen bg-grey grid grid-cols-7 py-20 lg:py-0 mt-6 lg:mt-16 px-8 md:px-16 lg:px-32 md:gap-8 lg:gap-10 justify-around md:justify-evenly items-center bg-[#f6f6f6]'>
-      <h1 className='w-fit px-5 md:px-10 py:1 md:py-2 absolute bg-[#fe4343] text-white rounded-tl-lg mt-0 md:mt-14 z-50 animate-bounce'><span className='animate-pulse'>Hottest Sale</span></h1>
+      <h1 className='w-fit px-5 md:px-10 py:1 md:py-2 absolute bg-[#fe4343] text-white rounded-tl-lg mt-0 md:mt-14 z-40 animate-bounce'><span className='animate-pulse'>Hottest Sale</span></h1>
       <div className='col-span-full lg:col-span-3 lg:py-6 flex flex-col justify-between lg:justify-evenly h-[300px] lg:h-[500px] z-40 order-last lg:order-first lg:text-left text-justify'>
         <h1 className='text-4xl md:text-5xl lg:text-7xl font-black tracking-wide'>Select Your New and Perfect Style</h1>
         <p className=''>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore odio tenetur illum doloremque officiis labore, pariatur molestiae dolorum alias placeat numquam, facilis repudiandae soluta maiores. Quidem molestiae exercitationem sunt illo!</p>
